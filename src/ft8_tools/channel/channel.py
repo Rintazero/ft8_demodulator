672aa21e37fb44bb827c537d220b390c24f7a1c5
szy_sice <szy_sice@bupt.edu.cn>
2025-03-29 12:30:13 +0800

    feat: add overhead prediction

--- src/ft8_tools/channel/channel.py
+++ src/ft8_tools/channel/channel.py
@@ -4,9 +4,12 @@ import skyfield.api
 import numpy
 import sgp4.api
 import datetime
+import folium
+import matplotlib.pyplot as plt
+import os
 from pymap3d.aer import eci2aer
 from pymap3d.eci import eci2ecef
-from pymap3d.ecef import ecef2eci,geodetic2ecef,geodetic2eci
+from pymap3d.ecef import geodetic2ecef,geodetic2eci,eci2geodetic
 from pymap3d.sidereal import datetime2sidereal
 import numpy as np
 
@@ -57,7 +60,7 @@ class Channel:
         c = 299792458
         eci_pos = self.groundStation.get_ground_station_position_eci(timestamp)
         gSta_pos_ecef = self.groundStation.get_ground_station_position_ecef(timestamp)
-        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second)
+        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second+timestamp.microsecond/1e6)
         e,r,v = self.satellite.sgp4(jd, fr)
         
         r = numpy.array(r) * 1e3
@@ -78,7 +81,7 @@ class Channel:
     def calculate_normalized_doppler_frequency_shift_by_eci(self,timestamp: datetime.datetime):
         c = 299792458
         eci_pos = self.groundStation.get_ground_station_position_eci(timestamp)
-        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second)
+        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second+timestamp.microsecond/1e6)
         e,r,v = self.satellite.sgp4(jd, fr)
         
         r = numpy.array(r) * 1e3
@@ -94,7 +97,7 @@ class Channel:
     
     def calculate_elevation_groundStation_to_satellite(self,timestamp: datetime.datetime):
         # eci_pos = self.groundStation.get_ground_station_position_eci(timestamp)
-        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second)
+        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second+timestamp.microsecond/1e6)
         e,r,v = self.satellite.sgp4(jd, fr)
 
         r = numpy.array(r) * 1e3
@@ -103,6 +106,170 @@ class Channel:
         
         return el
 
+    def get_satellite_star_point(self,timestamp: datetime.datetime):
+        jd, fr = sgp4.api.jday(timestamp.year,timestamp.month,timestamp.day,timestamp.hour,timestamp.minute,timestamp.second+timestamp.microsecond/1e6)
+        e,r,v = self.satellite.sgp4(jd, fr)
+        r = numpy.array(r) * 1e3
+        satellite_geodetic_pos = eci2geodetic(r[0],r[1],r[2],timestamp)
+        return satellite_geodetic_pos
+
+    def get_orbital_period(self) -> float:
+        """
+        计算卫星轨道周期（单位：分钟）
+        从 TLE 数据中的平均运动(mean motion)计算
+        """
+        # 从 TLE 第二行获取平均运动（rev/day）
+        mean_motion = float(self.satelliteTLE["TLE_line2"][52:63])
+        
+        # 计算周期（分钟）
+        # 平均运动是每天绕地球的圈数
+        # 周期 = 24小时 / 平均运动
+        period_minutes = 24 * 60 / mean_motion
+        
+        return period_minutes
+
+    def satellite_overhead_time_prediction(self, start_time: datetime.datetime, end_time: datetime.datetime, elevation_threshold_deg: float):
+        # 计算卫星在给定时间范围内的所有过顶时间
+        # 输入: 开始时间, 结束时间, 最小仰角阈值
+        # 输出: 过顶时间列表
+        
+        # 1. 获取卫星轨道数据
+        candidates = []
+        delta_t = datetime.timedelta(minutes = 1)
+        delta_tt = datetime.timedelta(seconds = 1)
+        t = start_time
+        t_enter = t
+        t_leave = t
+        overhead_time_minutes = 0
+        max_elevation = -90
+        while t < end_time:
+            max_elevation = -90
+            elevation = self.calculate_elevation_groundStation_to_satellite(t)
+            if elevation > elevation_threshold_deg:
+                t_enter = t
+                while elevation > elevation_threshold_deg:
+                    t_enter -= delta_tt
+                    elevation = self.calculate_elevation_groundStation_to_satellite(t_enter)
+                    max_elevation = max(max_elevation,elevation)
+                t_enter += delta_tt
+                t_leave = t + delta_tt
+                elevation = self.calculate_elevation_groundStation_to_satellite(t_leave)
+                while elevation > elevation_threshold_deg:
+                    t_leave += delta_tt
+                    elevation = self.calculate_elevation_groundStation_to_satellite(t_leave)
+                    max_elevation = max(max_elevation,elevation)
+                t_leave -= delta_tt
+                overhead_time_minutes = t_leave - t_enter
+                candidates.append((t_enter,overhead_time_minutes,max_elevation))
+                t = t_leave
+            t += delta_t
+
+        # 2. 计算卫星在给定时间范围内的所有过顶时间
+        candidates.sort(key=lambda x: x[2], reverse=True)
+        return candidates
+
+    def get_satellite_star_point_map_by_folium(self,start_time: datetime.datetime, num_samples: int, delta_t: datetime.timedelta, max_num_draw_points: int = 100, is_save_fig: bool = False, save_fig_path: str = None):
+        satellite_geodetic_pos_seq = []
+        for i in range(num_samples):
+            timestamp = start_time + delta_t * i
+            satellite_geodetic_pos = self.get_satellite_star_point(timestamp)
+            satellite_geodetic_pos_seq.append(satellite_geodetic_pos)
+
+        # Create a map centered around the first satellite position
+        initial_position = satellite_geodetic_pos_seq[0]
+        satellite_map = folium.Map(location=[initial_position[0], initial_position[1]], zoom_start=6)
+
+        # Create a marker for the starting position of the satellite
+        folium.Marker(location=[initial_position[0], initial_position[1]], 
+                      popup=f'Start Position\nTime: {start_time}', 
+                      icon=folium.Icon(color='green')).add_to(satellite_map)
+
+        # Create a marker for the ending position of the satellite
+        final_position = satellite_geodetic_pos_seq[-1]
+        folium.Marker(location=[final_position[0], final_position[1]], 
+                      popup=f'End Position\nTime: {start_time + delta_t * num_samples}', 
+                      icon=folium.Icon(color='red')).add_to(satellite_map)
+        
+        # Create a marker for the ground station position
+        folium.Marker(location=[self.groundStation.latitude_deg, self.groundStation.longitude_deg], 
+                      popup='Ground Station', 
+                      icon=folium.Icon(color='blue')).add_to(satellite_map)
+        
+        # Draw points directly on the map with a limit on the number of points
+        step = max(1, len(satellite_geodetic_pos_seq) // max_num_draw_points)  # Calculate step size for even distribution
+        for i in range(0, len(satellite_geodetic_pos_seq), step):
+            pos = satellite_geodetic_pos_seq[i]
+            folium.CircleMarker(location=[pos[0], pos[1]], radius=1, color='blue', fill=True, fill_color='blue', fill_opacity=0.6).add_to(satellite_map)
+
+        if is_save_fig:
+            # Create directory if it doesn't exist
+            os.makedirs(save_fig_path, exist_ok=True)
+            # Save the map
+            satellite_map.save(os.path.join(save_fig_path, 'satellite_star_point_map.html'))
+
+    def get_overhead_prediction_candidate_info(self, start_time: datetime.datetime, duration: datetime.timedelta, is_save_fig: bool = False, save_fig_path: str = None):
+        delta_t = datetime.timedelta(seconds=1)
+        t = start_time
+        
+        normalized_doppler_frequency_shift_seq = []
+        elevation_seq = []
+        
+        while t < start_time + duration:
+            normalized_doppler_frequency_shift = self.calculate_normalized_doppler_frequency_shift_by_ecef(t)
+            elevation = self.calculate_elevation_groundStation_to_satellite(t)
+            
+            normalized_doppler_frequency_shift_seq.append(normalized_doppler_frequency_shift)
+            elevation_seq.append(elevation)
+            
+            t += delta_t
+        
+        # Visualization of normalized_doppler_frequency_shift and elevation
+        fig, ax1 = plt.subplots()
+        
+        # Plot normalized_doppler_frequency_shift
+        ax1.set_xlabel('Time (s)')
+        ax1.set_ylabel('Normalized Doppler Frequency Shift', color='tab:blue')
+        ax1.plot([i for i in range(len(normalized_doppler_frequency_shift_seq))], normalized_doppler_frequency_shift_seq, color='tab:blue', label='Normalized Doppler Frequency Shift')
+        ax1.tick_params(axis='y', labelcolor='tab:blue')
+        
+        # Create a second y-axis for elevation
+        ax2 = ax1.twinx()
+        ax2.set_ylabel('Elevation (degrees)', color='tab:red')
+        ax2.plot([i for i in range(len(elevation_seq))], elevation_seq, color='tab:red', label='Elevation')
+        ax2.tick_params(axis='y', labelcolor='tab:red')
+        
+        plt.title('Normalized Doppler Frequency Shift and Elevation Over Time')
+        fig.tight_layout()
+        plt.grid()
+        if is_save_fig:
+            # Create directory if it doesn't exist
+            os.makedirs(save_fig_path, exist_ok=True)
+            candidate_info_path = os.path.join(save_fig_path, 'overhead_prediction_candidate_info.txt')
+            with open(candidate_info_path, 'w') as f:
+                f.write("Overhead Prediction Candidate Info\n")
+                f.write("----------------------------------\n")
+                f.write("Satellite Info\n")
+                f.write(f"Satellite Name: {self.satelliteTLE['name']}\n")
+                f.write(f"Satellite TLE Line 1: {self.satelliteTLE['TLE_line1']}\n")
+                f.write(f"Satellite TLE Line 2: {self.satelliteTLE['TLE_line2']}\n")
+                f.write("----------------------------------\n")
+                f.write("Ground Station Info\n")
+                f.write(f"Ground Station Name: {self.groundStation.name}\n")
+                f.write(f"Ground Station Latitude: {self.groundStation.latitude_deg}\n")
+                f.write(f"Ground Station Longitude: {self.groundStation.longitude_deg}\n")
+                f.write(f"Ground Station Altitude: {self.groundStation.altitude_m}\n")
+                f.write("----------------------------------\n")
+                f.write("Overhead Prediction Candidate Info\n")
+                f.write(f"Start Time: {start_time}\n")
+                f.write(f"Duration: {duration}\n")
+                
+            # Save the plot
+            plt.savefig(os.path.join(save_fig_path, 'normalized_doppler_frequency_shift_and_elevation_over_time.png'))
+            # Save the map
+            self.get_satellite_star_point_map_by_folium(start_time, duration // delta_t, delta_t, duration // delta_t, is_save_fig, save_fig_path)
+        else:
+            plt.show()
+        
 
 def eci2ecef_velocity(v_eci, time):
     """
--- src/tests/channel/test_channel.py
+++ src/tests/channel/test_channel.py
@@ -2,7 +2,7 @@ import datetime
 import numpy
 import sys
 import os
-
+import math
 # Add src directory to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from ft8_tools.channel.channel import Channel
@@ -15,32 +15,49 @@ satelliteTLE = {
 
 groundStation = {
     "name": "Station",
-    "latitude_deg": 20.7634433315784,
-    "longitude_deg": 116.560494091634,
+    "latitude_deg": 20.75046789797617,
+    "longitude_deg": 116.55005431954011,
     "altitude_m": 0,
 }
 
 channel = Channel(groundStation,satelliteTLE)
 
 
-StartTime = datetime.datetime(year = 2024, month = 5, day = 31, hour = 16, minute = 5, second = 51)
+StartTime = datetime.datetime(year = 2024, month = 5, day = 31, hour = 16, minute = 5, second = 51, microsecond = 0)
 
-duration_s = 2000 * 10
-delta_t_s = 10
-num_samples = int(duration_s / delta_t_s)
+delta_t_us = 10000
+duration_us = 12640 * delta_t_us
+
+num_samples = int(duration_us / delta_t_us)
 
 normalized_doppler_frequency_shift_seq = numpy.zeros(num_samples)
 elevation_deg_groundStation_to_satellite_seq = numpy.zeros(num_samples)
 
+satellite_period_minutes = channel.get_orbital_period()
+print(f"卫星轨道周期: {satellite_period_minutes} 分钟")
+
 for i in range(num_samples):
     # i = i - num_samples // 2
-    t = StartTime - datetime.timedelta(seconds = (i - num_samples // 2) * delta_t_s)
+    t = StartTime - datetime.timedelta(microseconds = (i - num_samples // 2) * delta_t_us)
 
     normalized_doppler_frequency_shift_by_ecef = channel.calculate_normalized_doppler_frequency_shift_by_ecef(t)
     elevation_deg_groundStation_to_satellite = channel.calculate_elevation_groundStation_to_satellite(t)
 
     normalized_doppler_frequency_shift_seq[i] = normalized_doppler_frequency_shift_by_ecef
     elevation_deg_groundStation_to_satellite_seq[i] = elevation_deg_groundStation_to_satellite
+
+satellite_geodetic_pos = channel.get_satellite_star_point(StartTime)
+print(satellite_geodetic_pos)
+
+satellite_overhead_time_prediction_candidates = channel.satellite_overhead_time_prediction(StartTime, StartTime + datetime.timedelta(days = 1), 10)
+print(satellite_overhead_time_prediction_candidates)
+
+selected_candidate = satellite_overhead_time_prediction_candidates[1]
+
+timeDelta = datetime.timedelta(minutes = 1)
+channel.get_satellite_star_point_map_by_folium(selected_candidate[0], 7, timeDelta, max_num_draw_points=num_samples//20, is_save_fig=False)
+
+channel.get_overhead_prediction_candidate_info(selected_candidate[0], selected_candidate[1], is_save_fig=True, save_fig_path="./src/tests/channel/overhead_prediction_candidate_info/")
 
 # 可视化 normalized_doppler_frequency_shift 和 elevation_deg_groundStation_to_satellite 随时间的变化
 import matplotlib.pyplot as plt
@@ -49,15 +66,15 @@ import matplotlib.pyplot as plt
 fig, ax1 = plt.subplots()
 
 # Plot normalized_doppler_frequency_shift
-ax1.set_xlabel('Time (s)')
+ax1.set_xlabel('Time (us)')
 ax1.set_ylabel('Normalized Doppler Frequency Shift', color='tab:blue')
-ax1.plot([i * delta_t_s for i in range(num_samples)], normalized_doppler_frequency_shift_seq, color='tab:blue', label='Normalized Doppler Frequency Shift')
+ax1.plot([i * delta_t_us for i in range(num_samples)], normalized_doppler_frequency_shift_seq, color='tab:blue', label='Normalized Doppler Frequency Shift')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
 
 # Create a second y-axis for elevation
 ax2 = ax1.twinx()
 ax2.set_ylabel('Elevation (degrees)', color='tab:red')
-ax2.plot([i * delta_t_s for i in range(num_samples)], elevation_deg_groundStation_to_satellite_seq, color='tab:red', label='Elevation')
+ax2.plot([i * delta_t_us for i in range(num_samples)], elevation_deg_groundStation_to_satellite_seq, color='tab:red', label='Elevation')
 ax2.tick_params(axis='y', labelcolor='tab:red')
 
 # Title and grid
@@ -67,7 +84,6 @@ plt.grid()
 
 # Show the plot
 plt.show()
-
 
 # normalized_doppler_frequency_shift_by_eci = channel.calculate_normalized_doppler_frequency_shift_by_eci(t)
 
